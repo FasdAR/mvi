3b988cd376c7cfe1e4f17f1a44627776b317ef4a
Andrey Rednikov <andreyrednikov@gmail.com>
2021-02-19 20:45:59 +0300

    Edited dependency

--- mvi/build.gradle.kts
+++ mvi/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     buildToolsVersion("30.0.3")
 
     defaultConfig {
-        minSdkVersion(24)
+        minSdkVersion(21)
         targetSdkVersion(30)
         versionCode = 1
         versionName = "1.0"
@@ -25,8 +25,4 @@ android {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.30")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.0")
 }
