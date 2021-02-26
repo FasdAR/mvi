7db78df6d5416fd1ecf88d6812cd9aeaa3e9782f
Andrey Rednikov <andreyrednikov@gmail.com>
2021-02-26 11:26:16 +0300

    Added bottom dialog screen

--- mvi/src/main/java/ru/fasdev/mvi/core/Store.kt
+++ mvi/src/main/java/ru/fasdev/mvi/core/Store.kt
@@ -1,14 +1,17 @@
 package ru.fasdev.mvi.core
 
+import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.launch
+import ru.fasdev.mvi.BuildConfig
 import ru.fasdev.mvi.core.action.Action
 import ru.fasdev.mvi.core.state.UiEffect
 import ru.fasdev.mvi.core.state.UiState
@@ -48,12 +51,29 @@ class Store<S : UiState, E : UiEffect>(
     @Suppress("UNCHECKED_CAST")
     private suspend fun initReduce() {
         actionBus
-            .map { reducer.reduce(it, currentState) }
+            .map {
+                val value = state.value
+                val first = state.first()
+
+                Log.d("STATES", "$value  |||  $first")
+
+                val newState = reducer.reduce(it, currentState)
+
+                if (BuildConfig.DEBUG) {
+                    Log.d("SCAN_STATE", "Current State: $currentState, Action: $it, New State: $newState")
+                }
+
+                newState
+            }
             .collect {
                 it.forEach {
                     when (it) {
                         is UiState -> {
                             val newState = it as S
+                            if (BuildConfig.DEBUG) {
+                                Log.d("UI_STATE_COLLECT", "Current State: $currentState, New State: $newState")
+                            }
+
                             if (currentState != newState)
                                 state.value = newState
                         }
